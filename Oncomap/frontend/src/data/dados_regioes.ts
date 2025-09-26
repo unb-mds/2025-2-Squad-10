87bc1085d448083bebc23055533dd3e9b8ebbd8e
Gotc2607 <giovanicoelho2005@gmail.com>
2025-09-26 12:03:16 -0300

    feat: criando um arquivo de teste para testar o TabelaInfo

---
+++ Oncomap/frontend/src/data/dados_regioes.ts
@@ -0,0 +1,47 @@
+// src/data/dadosRegioes.ts
+import { DadosRegiao } from '../components/MapaPage/TabelaInfo';
+
+// Um objeto que mapeia o nome da região para seus dados
+export const dadosDasRegioes: Record<string, DadosRegiao> = {
+  norte: {
+    regiao: 'Norte',
+    investimentos: [
+      { nome: 'Medicamentos', valor: 'R$ 15.200.000,00' },
+      { nome: 'Equipamentos', valor: 'R$ 8.750.000,00' },
+    ],
+    municipios: ['Manaus', 'Belém', 'Porto Velho', 'Macapá', 'Rio Branco'],
+  },
+  nordeste: {
+    regiao: 'Nordeste',
+    investimentos: [
+      { nome: 'Medicamentos', valor: 'R$ 35.800.000,00' },
+      { nome: 'Infraestrutura', valor: 'R$ 52.100.000,00' },
+    ],
+    municipios: ['Salvador', 'Recife', 'Fortaleza', 'São Luís', 'Maceió'],
+  },
+  // Adicione aqui os dados para 'sul', 'sudeste' e 'centroOeste'
+  sul: { 
+    regiao: 'Sul',
+    investimentos: [
+      { nome: 'Medicamentos', valor: 'R$ 35.800.000,00' },
+      { nome: 'Infraestrutura', valor: 'R$ 52.100.000,00' },
+    ],
+    municipios: ['Salvador', 'Recife', 'Fortaleza', 'São Luís', 'Maceió'],
+  },
+  sudeste: { 
+    regiao: 'Sudeste',
+    investimentos: [
+      { nome: 'Medicamentos', valor: 'R$ 35.800.000,00' },
+      { nome: 'Infraestrutura', valor: 'R$ 52.100.000,00' },
+    ],
+    municipios: ['Salvador', 'Recife', 'Fortaleza', 'São Luís', 'Maceió'],
+    },
+  centroOeste: { 
+    regiao: 'Sudeste',
+    investimentos: [
+      { nome: 'Medicamentos', valor: 'R$ 35.800.000,00' },
+      { nome: 'Infraestrutura', valor: 'R$ 52.100.000,00' },
+    ],
+    municipios: ['Salvador', 'Recife', 'Fortaleza', 'São Luís', 'Maceió'],
+   },
+};
