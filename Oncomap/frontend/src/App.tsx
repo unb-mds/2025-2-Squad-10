0cbcf1491a0946ad2fe4587c066516a64297960b
Gotc2607 <giovanicoelho2005@gmail.com>
2025-09-26 11:41:32 -0300

    mudando a pasta data para dentro de src

--- Oncomap/frontend/src/App.tsx
+++ Oncomap/frontend/src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import HomePage from './pages/HomePage'
 import MapaPege from "./pages/MapaPage"
 import Layout from "./components/Geral/layout_sidebar"
-import TestePage from "./pages/Teste"
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
         <Route path="/" element={<HomePage/>}/>
         <Route element={<Layout />}>
           <Route path="/mapa" element={<MapaPege/>}/>
-          <Route path="/teste" element={<TestePage/>}/>
           /* adicionar outras rotas quando as paginas estiverem prontas */
         </Route>
       </Routes>
