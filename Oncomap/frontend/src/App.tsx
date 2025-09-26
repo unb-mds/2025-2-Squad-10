3ceceae88cd9716dd291abaa4222ecc71a84484f
Gotc2607 <giovanicoelho2005@gmail.com>
2025-09-26 11:37:51 -0300

    arrumando o App.tsx para conter as rotas e organisando os arquivo da TabelaInfo

--- Oncomap/frontend/src/App.tsx
+++ Oncomap/frontend/src/App.tsx
@@ -1,22 +1,23 @@
 
-import TabelaInfo from './components/TabelaInfo'; 
-import './App.css';
-import './index.css'; 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import HomePage from './pages/HomePage'
+import MapaPege from "./pages/MapaPage"
+import Layout from "./components/Geral/layout_sidebar"
+import TestePage from "./pages/Teste"
+
 function App() {
   return (
-    <div className="App">
-      <main className="App-content">
-        {}
-        <div className="mapa-placeholder">
-          <p>Área do Mapa</p>
-        </div>
-
-       {}
-        <TabelaInfo />
-        
-      </main>
-    </div>
-  );
+      <Router>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route element={<Layout />}>
+          <Route path="/mapa" element={<MapaPege/>}/>
+          <Route path="/teste" element={<TestePage/>}/>
+          /* adicionar outras rotas quando as paginas estiverem prontas */
+        </Route>
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
