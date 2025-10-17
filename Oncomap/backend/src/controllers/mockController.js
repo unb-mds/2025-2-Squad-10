43548faa2edf4a957300db760f06c2c2899fb8e2
Alexandroni07 <gaalexandroni@gmail.com>
2025-10-17 11:02:14 -0300

    feat: atualização do Mockcontroller

--- Oncomap/backend/src/controllers/mockController.js
+++ Oncomap/backend/src/controllers/mockController.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const path = require('path');
 
-// Função para ler nossos arquivos JSON de forma segura
+// Função auxiliar para ler o nosso mock
 const readMockFile = (fileName) => {
     try {
         const filePath = path.join(__dirname, '..', 'mocks', fileName);
@@ -14,35 +14,18 @@ const readMockFile = (fileName) => {
     }
 };
 
-const getRegionSummary = (req, res) => {
-    const { regionName } = req.params;
+// Controller que envia todos os dados de investimentos das regiões
+const getDadosInvestimentos = (req, res) => {
+    console.log('Requisição recebida para dados de investimentos...');
 
-    // Para a demo, vamos retornar os dados do Sudeste, não importa a região clicada
-    console.log(`Buscando dados mock para a região: ${regionName}`);
-
-    const data = readMockFile('summary_sudeste.json');
+    const data = readMockFile('dados_investimentos.json');
 
     if (data) {
         return res.status(200).json(data);
     }
-    return res.status(500).json({ error: 'Falha ao ler dados do resumo.' });
-};
-
-const getMunicipalityDetails = (req, res) => {
-    const { ibgeCode } = req.params;
-
-    // Para a demo, vamos retornar os dados do Rio, não importa a cidade clicada
-    console.log(`Buscando dados mock para o município com código IBGE: ${ibgeCode}`);
-
-    const data = readMockFile('details_rio.json');
-
-    if (data) {
-        return res.status(200).json(data);
-    }
-    return res.status(500).json({ error: 'Falha ao ler dados do município.' });
+    return res.status(500).json({ error: 'Falha ao ler dados de investimentos.' });
 };
 
 module.exports = {
-    getRegionSummary,
-    getMunicipalityDetails,
+    getDadosInvestimentos,
 };
