c863e764bafbfbb27e212eebd986db65cc87e978
joaoPedro-201 <joaopedroabc201@gmail.com>
2025-10-17 00:29:04 -0300

    build - Organizacao da estrutura das pastas

---
+++ Oncomap/backend/src/app.js
@@ -0,0 +1,23 @@
+// Oncomap/backend/src/app.js
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Importar as rotas (note o novo caminho relativo)
+const mapRoutes = require('./api/routes/mapRoutes');
+
+// Rota para verificar a saúde da API
+app.get('/api/health', (req, res) => {
+  res.status(200).json({ message: 'Backend está funcionando!' });
+});
+
+// Usar as Rotas com um prefixo de versão (boa prática)
+app.use('/api/v1/map', mapRoutes);
+
+// Exportamos o 'app' para que o server.js possa usá-lo
+module.exports = app;
